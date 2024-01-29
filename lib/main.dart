7f19a60dc66ee366ddc527fc24b026255ee9aa75
Vithushan N <vithushan@unicomtechnology.co.uk>
2024-01-29 18:09:17 -0500

    Removed unwanted var in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz.dart';
 
-const startAlignment = Alignment.topLeft;
-const endAlignment = Alignment.bottomRight;
-
 void main() {
   runApp(const Quiz());
 }
